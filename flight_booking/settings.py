fe8ac6dc7b5aaa2de2ae75fa226e30ff95e9fe8c
Ragini2212 <20BCS7891@cuchd.in>
2023-06-30 21:14:52 +0530

    deploying

--- flight_booking/settings.py
+++ flight_booking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-j@j0dxwlqs6rti7f6a5m2+#+!x+hy#ili3v$k2(9cjpe)c5%w5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -136,3 +136,7 @@ MEDIA_URL = '/images/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
